0013d9131c8aeb1bb396f5b657b317baeb4df8d4
effervescent000 <kislio01@gmail.com>
2023-07-03 13:19:55 -0400

    move header into its own file

--- src/components/calendar/calendar-frame.svelte
+++ src/components/calendar/calendar-frame.svelte
@@ -1,46 +1,18 @@
 <script lang="ts">
-	// PROPS
-
-	import Icon from '../icon.svelte';
+	import CalendarHeader from './calendar-header.svelte';
 	import Day from './day.svelte';
+	// PROPS
 
 	// STATE
 	let season = 0;
 
 	// LOGIC
 
-	const SEASONS = ['Spring', 'Summer', 'Fall', 'Winter'];
 	const DAYS_OF_WEEK = ['M', 'T', 'W', 'Th', 'F', 'Sa', 'Su'];
 	const dates = Array.from({ length: 28 }, (_x, i) => i);
 </script>
 
-<div class="w-[700px] text-center">
-	<span>
-		<Icon
-			key="caret-left"
-			callback={() => {
-				if (season === 0) {
-					season = 3;
-				} else {
-					season = season - 1;
-				}
-			}}
-		/>
-	</span>
-	<span>{SEASONS[season]}</span>
-	<span>
-		<Icon
-			key="caret-right"
-			callback={() => {
-				if (season === 3) {
-					season = 0;
-				} else {
-					season = season + 1;
-				}
-			}}
-		/>
-	</span>
-</div>
+<CalendarHeader {season} />
 <div class="grid grid-cols-7 border border-black w-[700px]">
 	{#each DAYS_OF_WEEK as day}
 		<span class="text-center">{day}</span>
---
+++ src/components/calendar/calendar-header.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Icon from '../icon.svelte';
+
+	// PROPS
+	export let season: number;
+
+	// STATE
+
+	// LOGIC
+	const SEASONS = ['Spring', 'Summer', 'Fall', 'Winter'];
+</script>
+
+<div class="w-[700px] text-center">
+	<span>
+		<Icon
+			key="caret-left"
+			callback={() => {
+				if (season === 0) {
+					season = 3;
+				} else {
+					season = season - 1;
+				}
+			}}
+		/>
+	</span>
+	<span>{SEASONS[season]}</span>
+	<span>
+		<Icon
+			key="caret-right"
+			callback={() => {
+				if (season === 3) {
+					season = 0;
+				} else {
+					season = season + 1;
+				}
+			}}
+		/>
+	</span>
+</div>
