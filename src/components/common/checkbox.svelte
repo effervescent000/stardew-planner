645d2a14156c249a58d826e640c57b5af7ea63c6
effervescent000 <kislio01@gmail.com>
2023-07-03 14:02:04 -0400

    create and render checkbox

---
+++ src/components/common/checkbox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// PROPS
+	export let checked: boolean;
+	export let callback = () => {};
+
+	// STATE
+
+	// LOGIC
+</script>
+
+<input type="checkbox" {checked} on:click={callback} />
